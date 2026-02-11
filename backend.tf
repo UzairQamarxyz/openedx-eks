67f542d4d9de614be59273c1274aa52dbf327659
Uzair Qamar <uzairqamarxyz@gmail.com>
2026-02-11 21:48:45 +0500

    Update the backend
    Signed-off-by: Uzair Qamar <uzairqamarxyz@gmail.com>

--- backend.tf
+++ backend.tf
@@ -30,16 +30,18 @@ provider "aws" {
 }
 
 provider "kubectl" {
-  apply_retry_count      = 5
+  apply_retry_count      = 15
   host                   = module.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority_data)
-  load_config_file       = true
+  load_config_file       = false
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "/bin/sh"
+    args = [
+      "-c",
+      "aws eks get-token --cluster-name ${module.eks_cluster.cluster_name} --output json"
+    ]
   }
 }
 
